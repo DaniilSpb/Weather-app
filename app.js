2cf64ba3e0b26b77d259c3228595b05d6f8e03cb
Daniil Elin <daniil.elin@mail.ru>
2019-10-27 20:34:21 +0300

    html code weather-app

---
+++ app.js
@@ -0,0 +1,7 @@
+// api key : febb7355330a85cf811de81505dd7611
+//SELECT ELEMENTS
+const iconElemet = document.querySelector(".weather-icon");
+const tempElemet = document.querySelector(".temperature-value p");
+const descElemet = document.querySelector(".temperature-description p");
+const locationElemet = document.querySelector(".location p");
+const notificationElemet = document.querySelector(".notification");
